96fedb120a7e814c6a9052be5dc1434eebe6fff3
Troy Sellers <tsellers@salesforce.com>
2017-10-04 15:18:09 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var request = require('request');
+
+/*
+Function will call the tokenService dyno to retrieve a valid access token for the given org_id
+*/
+exports.fetchToken = function (org_id, callback) {
+
+   var json = {
+      "org_id": org_id
+   };
+
+   var options = {
+      url: process.env.TOKEN_SERVICE,
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      json: json
+   };
+
+   request(options, function(err, res, body) {
+      if (err) {
+         console.log(err);
+         callback(err, null);
+      }
+      if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+         callback(null, body);
+      }
+   });
+}
